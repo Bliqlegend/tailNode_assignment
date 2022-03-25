47f44e972ffc6c28b504efbc123c81db563738ec
Tanush Yadav <tanushkumar244@gmail.com>
2022-03-25 10:14:45 +0530

    Still In Progress : Book Scraper :+1:

--- Part-B/scraping_bookstore.py
+++ Part-B/scraping_bookstore.py
@@ -6,7 +6,7 @@ def ScrapeCurrpage(myurl):
     uClient = uReq(myurl)
     page_html = uClient.read()
     uClient.close()
-
+    aval = False
     page_soup = soup(page_html, "html.parser")
 
     bookshelf = page_soup.findAll(
@@ -22,14 +22,24 @@ def ScrapeCurrpage(myurl):
     
         book_title = books.h3.a["title"]
         book_price = books.findAll("p", {"class": "price_color"})
-        book_availability = books.findAll("p", {"class": "price_color"})
-        book Ratings = books.findAll("p", {"class": "star-rating Three"})
+        book_availability = books.findAll("p", {"class": "instock availability"})
+        book_Ratings = books.findAll("p", {"class": "star-rating Three"})
+        # if "In Stock" in book_availability: 
+            # aval = True
+        print(book_availability)
         price = book_price[0].text.strip()
+        # if aval:
+        #     print("Availability : In Stock")
+        # else:
+        #     print("Availability : Not in Stock")
+
+        # print("Ratings")
+        # print(book_Ratings)
     
-        print("Title of the book :" + book_title)
-        print("Price of the book :" + price)
-        print("Availability of the book :" + price)
-        print("Rating of the book :" + price)
+        # print("Title of the book :" + book_title)
+        # print("Price of the book :" + price)
+        # print("Availability of the book :" + price)
+        # print("Rating of the book :" + price)
 
     
         f.write(book_title + "," + price+"\n")
@@ -43,3 +53,4 @@ for page_number in range(1,51):
     print(f"Page {page_number}")
     print("__________________________________________________________")
     ScrapeCurrpage(myurl)
+    break
---
+++ Part-B/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+if aval:
+        #     print("Availability : In Stock")
+        # else:
+        #     print("Availability : Not in Stock")
